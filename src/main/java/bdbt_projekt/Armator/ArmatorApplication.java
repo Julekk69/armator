db36cbeab8871ef924fe7d7e28bcb6d94cf6c243
blaskus <01169315@pw.edu.pl>
2023-01-07 18:45:53 +0100

    Initial commit

---
+++ src/main/java/bdbt_projekt/Armator/ArmatorApplication.java
@@ -0,0 +1,13 @@
+package bdbt_projekt.Armator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArmatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArmatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bdbt_projekt/Armator/ArmatorApplicationTests.java
@@ -0,0 +1,13 @@
+package bdbt_projekt.Armator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArmatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
